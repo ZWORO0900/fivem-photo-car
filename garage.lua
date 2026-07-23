2227194c5d3c7324f1c429bcc135fad64606a83a
ZWORO0900 <87627643+ZWORO0900@users.noreply.github.com>
2026-07-23 15:42:22 +0300

    VehCatalog: update garage (3 vehicles)

---
+++ garage.lua
@@ -0,0 +1,6 @@
+-- VehCatalog auto-generated
+return {
+    ["GODzLCTRD"] = {"GODzLCTRD",0,"<img src='https://raw.githubusercontent.com/ZWORO0900/fivem-photo-car/main/GODzLCTRD_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["ispolice1"] = {"ispolice1",0,"<img src='https://raw.githubusercontent.com/ZWORO0900/fivem-photo-car/main/ispolice1_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["ispolice2"] = {"ispolice2",0,"<img src='https://raw.githubusercontent.com/ZWORO0900/fivem-photo-car/main/ispolice2_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+}
